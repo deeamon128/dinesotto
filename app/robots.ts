649c8a3026cc0b55db82b2dc82d8e41fc3cf6c23
Andreea <andreeambobariu@gmail.com>
2026-05-17 16:32:20 +0100

    sitemap fix

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://dinesotto.com/sitemap.xml",
+    sitemap: "https://www.dinesotto.com/sitemap.xml",
   };
 }
